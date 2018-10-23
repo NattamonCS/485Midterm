617b9a3481d12f70096b83955cda68b5a4544d20
NattamonCS <tang.nataly@gmail.com>
2018-10-23 12:10:30 -0700

    2D Player Character Sprite
    Under Assets Folder:
    - In the Scene folder, 2D_TrainToMove.Unity  still cannot move. Waiting for script .cs code to make the player move.
    - In the Sprite2D_BackUp, there are the 2D sprite of the player to use in the final project.
    
    - In 2D_Demo folder, Try to write script into MoveSprite2D.Unity, but it still does not move. There are several error message.

---
+++ Temp/2D Character Sprite/Assets/2D_Demo/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    private bool facingRight = true; // True == Right, False == Left
+    public int playerJumpPower = 1250;
+    private float moveX;
+
+    private Rigidbody rb;
+
+    
+    void Start () // Use this for initialization
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization all the physics code
+    void FixedUpdate() // call before performing any physics calculations
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * playerSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove () {
+        // Controls
+        moveX = Input.GetAxis("Horizontal");
+       
+        // Animations
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        // Player direction
+        if (moveX > 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX < 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+    void Jump ()
+    {
+        // Jump
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer ()
+    {
+        facingRight = !facingRight;
+        Vector2 temp_localScale = gameObject.transform.localScale;
+        temp_localScale.x *= -1;
+        transform.localScale = temp_localScale;
+    }
+
+}
+
